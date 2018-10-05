719ab8015a17583a3d912faddc625ecbd2780538
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 22:12:26 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,2 +1,16 @@
 
+#named rawdata dataset as dfStates
 dfStates <- rawdata
+
+MyMode <- function(dfStates)
+{
+  dfStates <- dfStates[-1,]
+  num.row <- nrow(dfStates)
+  dfStates <- dfStates[-num.row,]
+  dfStates <- dfStates[,-1:-4]
+  colnames(dfStates) <- c("stateName", "population", "popOver18","percentOver18")
+  return(dfStates)
+}
+dfStates <- MyMode(dfStates)
+#provides clean dataset
+dfStates
