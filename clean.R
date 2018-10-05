26ff862f79d98a98fa843313c755875a8db87e33
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 22:16:19 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -6,8 +6,9 @@ dfStates <- rawdata
 #created function to clean the dataframe
 MyMode <- function(dfStates)
 {
-# removes first row because it is total for US, we do not need
+  #removes first row because it is total for US, we do not need
   dfStates <- dfStates[-1,]
+  #last row is Puerto Rico and should be removed
   num.row <- nrow(dfStates)
   dfStates <- dfStates[-num.row,]
   dfStates <- dfStates[,-1:-4]
