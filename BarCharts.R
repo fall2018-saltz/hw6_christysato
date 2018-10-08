1403193182aed9514e199c63aaca57072209f8b0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 17:43:07 -0400

    Saving BarCharts.R

--- BarCharts.R
+++ BarCharts.R
@@ -3,3 +3,7 @@
 #created a new dataframe called numMurders that shows the stateName and the corresponding Murder rate for that state
 numMurders <- data.frame(USArrestsdf$stateName, USArrestsdf$Murder)
 numMurders
+
+#STEPC8
+m_bar <- ggplot() +geom_col(data=numMurders, aes(x=USArrestsdf$stateName, y= USArrestsdf$Murder)) #credit: https://rpubs.com/sediaz/geom_col_1
+m_bar
