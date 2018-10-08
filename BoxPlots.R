855f02f63c16ee7c4d3e4b98e35bc3119319d0d8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 17:02:15 -0400

    Saving BoxPlots.R

--- BoxPlots.R
+++ BoxPlots.R
@@ -2,6 +2,6 @@
 library("ggplot2")
 #STEPB5
 #boxplot for population
-p_box <- ggplot(USArrestsdf, aes(y=population)) + 
+p_box <- ggplot(p, aes(y=population)) + 
   geom_boxplot(fill="slateblue")
 p_box
