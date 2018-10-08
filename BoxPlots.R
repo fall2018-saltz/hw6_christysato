745de8a917f3ad28a97841f544109c0d48d257e7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 17:09:06 -0400

    Saving BoxPlots.R

--- BoxPlots.R
+++ BoxPlots.R
@@ -16,3 +16,4 @@ m_box
 #the boxplot was more helpful than the histogram because the boxplot
 #illustrates the mean, max, min, median, first and third quartile 
 #in a better visual. It is easier for me to distinguish that in the
+#boxplot, rather than the histogram.
