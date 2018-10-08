ed00fc87b4899f0e3981be2b2a5c582c347e3aea
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 17:49:24 -0400

    Saving BarCharts.R

--- BarCharts.R
+++ BarCharts.R
@@ -4,6 +4,7 @@ library("ggplot2")
 #STEPC7
 #created a new dataframe called numMurders that shows the stateName and the corresponding Murder rate for that state
 numMurders <- data.frame(USArrestsdf$stateName, USArrestsdf$Murder)
+#shows two columns, left showing state names and the right column shows the corresponding Murder Rate
 numMurders
 
 #STEPC8
@@ -11,6 +12,6 @@ m_bar <- ggplot() +geom_col(data=numMurders, aes(x=USArrestsdf$stateName, y= USA
 m_bar
 
 #STEPC9
-m_bar <- m_bar + theme(axis.text.x = element_text(angle=90, hjust = 1))  #credit: https://stackoverflow.com/questions/1330989/rotating-and-spacing-axis-labels-in-ggplot2
-m_bar <- m_bar + ggtitle("Total Murders")
-m_bar
+m_barClean <- m_bar + theme(axis.text.x = element_text(angle=90, hjust = 1))  #credit: https://stackoverflow.com/questions/1330989/rotating-and-spacing-axis-labels-in-ggplot2
+m_barClean <- m_bar + ggtitle("Total Murders")
+m_barClean
