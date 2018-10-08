050a1b316b184e32ca725542ed0b5314f9f6c07b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 17:43:30 -0400

    Saving BarCharts.R

--- BarCharts.R
+++ BarCharts.R
@@ -1,4 +1,6 @@
 
+library("ggplot2")
+
 #STEPC7
 #created a new dataframe called numMurders that shows the stateName and the corresponding Murder rate for that state
 numMurders <- data.frame(USArrestsdf$stateName, USArrestsdf$Murder)
