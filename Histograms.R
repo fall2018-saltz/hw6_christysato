e669f16ea3dfe05cf71c918a76c6656ff229925b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 16:19:21 -0400

    Saving Histograms.R

--- Histograms.R
+++ Histograms.R
@@ -1,1 +1,10 @@
 
+library("ggplot2")
+#make histogram for population
+#creates ggplot called g, using the dataframe USArrestsdf, and grabbing the x axis as population
+g <- ggplot(USArrestsdf, aes(x=population))
+#make histogram with bars being white with black outline
+g <- g + geom_histogram(binwidth = 5000000, color="black", fill="white")
+#add a title to the histogram
+g <- g + ggtitle("Population of States")
+g
