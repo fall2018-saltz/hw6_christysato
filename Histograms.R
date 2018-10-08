4b9a8f3cbf7d05caffa46ca622100bdac4ffccc9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 16:30:51 -0400

    Saving Histograms.R

--- Histograms.R
+++ Histograms.R
@@ -10,3 +10,22 @@ g <- g + geom_histogram(binwidth = 5000000, color="black", fill="white")
 #add a title to the histogram
 g <- g + ggtitle("Population of States")
 g
+
+#make histogram for murder rate
+#create a ggplot called m, using the dataframe USArrestsdf, and grabbing the x axis as Murder
+m <- ggplot(USArrestsdf, aes(x=Murder))
+#make histogram with bars being white with black outline
+#had to change the binwidth, because did not show anything when it was equal to 5000000
+m <- m + geom_histogram(binwidth = 2, color="black", fill="white")
+#add a title to the histogram
+m <- m + ggtitle("Murder Rate")
+m
+
+#make histogram for assault
+#create a ggplot called a, using the dataframe USArrestsdf, and grabbing the x axis as Assault
+a <- ggplot(USArrestsdf, aes(x=Assault))
+#make histogram with bars being white with black outline
+a <- a + geom_histogram(binwidth = 10, color="black", fill="white")
+#add a title to the histogram
+a <- a + ggtitle("Assault Rate")
+a
