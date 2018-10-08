0e95e629de788c7eaf8587ccae6daf70ecda27db
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 17:04:15 -0400

    Saving BoxPlots.R

--- BoxPlots.R
+++ BoxPlots.R
@@ -7,6 +7,7 @@ p_box <- ggplot(USArrestsdf, aes(y=population)) +
   geom_boxplot(fill="slateblue")
 p_box
 
+#boxplot for muder
 m_box <- ggplot(USArrestsdf, aes(y=Murder)) +
   geom_boxplot(fill="slateblue")
 m_box
