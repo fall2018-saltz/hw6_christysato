b57f93f13bf8261b42d279392a1a8ed810cc59e2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 17:47:19 -0400

    Saving BarCharts.R

--- BarCharts.R
+++ BarCharts.R
@@ -10,4 +10,7 @@ numMurders
 m_bar <- ggplot() +geom_col(data=numMurders, aes(x=USArrestsdf$stateName, y= USArrestsdf$Murder)) #credit: https://rpubs.com/sediaz/geom_col_1
 m_bar
 
-
+#STEPC9
+m_bar <- m_bar + theme(axis.text.x = element_text(angle=90, hjust = 1))  #credit: https://stackoverflow.com/questions/1330989/rotating-and-spacing-axis-labels-in-ggplot2
+m_bar <- m_bar + ggtitle("Total Murders")
+m_bar
