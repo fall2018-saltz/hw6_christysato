5c927c1041abece011ddd496545fa50c760fa4ba
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 17:41:45 -0400

    deleting BarCharts.R

--- BarCharts.R
+++
@@ -1,5 +0,0 @@
-
-#STEPC7
-#created a new dataframe called numMurders that shows the stateName and the corresponding Murder rate for that state
-numMurders <- data.frame(USArrestsdf$stateName, USArrestsdf$Murder)
-numMurders
