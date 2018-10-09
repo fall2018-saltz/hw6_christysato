6d157bcd4f19d4e86b9ea31e0059b4bb49ca200d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 17:15:01 -0400

    Saving BoxPlots.R

--- BoxPlots.R
+++ BoxPlots.R
@@ -1,13 +1,14 @@
 
+#activate ggplot2 package
 library("ggplot2")
 
 #STEPB5
-#boxplot for population
+#boxplot for population with y-axis as population
 p_box <- ggplot(USArrestsdf, aes(y=population)) + 
   geom_boxplot(fill="slateblue")  #credit: https://www.r-graph-gallery.com/262-basic-boxplot-with-ggplot2/
 p_box
 
-#boxplot for muder
+#boxplot for murder
 m_box <- ggplot(USArrestsdf, aes(y=Murder)) +
   geom_boxplot(fill="slateblue")
 m_box
