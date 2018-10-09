8cec80f086307bf12e9b61d48cdc441bc1975bf1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-08 23:01:33 -0400

    Saving scatterPlot.R

--- scatterPlot.R
+++ scatterPlot.R
@@ -5,3 +5,5 @@ library("ggplot2")
 #created the scatter plot with x-axis as population, percentOver18 as y-axis, and size and color of dots as Murder rate
 scatter_plot <- ggplot(USArrestsdf, aes(x=population, y=percentOver18)) +geom_point(aes(size=Murder, color=Murder))
 scatter_plot
+
+#credit: http://www.sthda.com/english/wiki/ggplot2-scatter-plots-quick-start-guide-r-software-and-data-visualization
